64f6344353aca1ed1488133a1fdfb22d9a626d69
Sara Cortes <scortesccl@gmail.com>
2023-04-02 23:58:40 +0200

    heroes

---
+++ 07-heroes-spa/src/heroes/components/HeroCard.jsx
@@ -0,0 +1,46 @@
+import { Link } from "react-router-dom"
+
+const CharactersByHero = ({alter_ego,  characters}) => {
+    // if (alter_ego === characters) return (<></>)
+    // return <p> { characters } </p>
+    return (alter_ego === characters) 
+        ? <></>
+        : <p> { characters } </p>
+}
+
+export const HeroCard = ( {hero} ) => {
+
+    const heroImageUrl = `/assets/heroes/${ hero.id}.jpg`
+
+    // const charactersByHero = (<p>{hero.characters}</p>)
+    return (
+        <div className="col animate__animated animate__fadeInLeft">
+            <div className="card">
+                <div className="row no-gutters">
+                    <div className="col-4">
+                        <img src={heroImageUrl} className="card-img" alt={ hero.superhero} />
+                    </div>
+                    <div className="col-8">
+                        <div className="card-body">
+                            <h5 className="card-title">{hero.superhero}</h5>
+                            <p className="card-text">{hero.alter_ego}</p>
+
+                            {/* {
+                                (hero.alter_ego !== hero.characters) && charactersByHero
+                            } */}
+                            
+                            <CharactersByHero characters={hero.characters} alter_ego={hero.alter_ego} />
+
+                            <p className="card-text">
+                                <small className="text-muted">{hero.first_appearance}</small>
+                            </p>
+                            <Link to={`/hero/${hero.id}`}>
+                                Más ...
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
